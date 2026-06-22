e70f1a0daa60e7ea101cd61b1c4128c61bb4fe1f
Zaky Atha Aryansyah <zaky.gaul90@gmail.com>
2026-06-22 13:47:57 +0700

    [tahap 1] membuat database dan struktur tabel

--- db_uas_pbo_ti1c_zakyathaaryansyah.sql
+++ db_uas_pbo_ti1c_zakyathaaryansyah.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 22, 2026 at 06:11 AM
+-- Generation Time: Jun 22, 2026 at 06:45 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
